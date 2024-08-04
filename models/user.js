caf222ce62e715c1058109dbc6608ff485d23b7c
mohammad shoaib <mohammadshoaib7221@gmail.com>
2024-08-04 09:45:48 +0530

    update

--- models/user.js
+++ models/user.js
@@ -3,21 +3,21 @@ const mongoose = require("mongoose");
 // Define the schema
 const UserSchema = new mongoose.Schema({
   profilepic: {
-    
     type: String,
     required: true,
-    default : '/images/default.png'
-    
+    default: "/images/default.png",
   },
   username: { type: String, required: true },
   name: String,
   age: Number,
   email: { type: String, required: true },
   password: { type: String, required: true },
-  post: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post'
-  }]
+  post: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
 });
 
 // Register the model
